1b95006f8597c36d502128ec3736e00c0c4c978a
Jordan Lenard <leonard_2010@live.com>
2023-08-21 20:58:56 -0500

    added example to use for library

--- library.js
+++ library.js
@@ -1,4 +1,11 @@
-const myLibrary = [];
+const myLibrary = [
+    {
+        title: "Rich Dad Poor Dad",
+        author: "Robert Kiyosaki",
+        pages: 387,
+        read: true
+    }
+];
 
 function Book() {
 
